dda374a7eef22f23e4ce9815b8582c560065d739
Jason H <tapedubz@gmail.com>
2022-08-29 21:34:36 +0100

    Final bits of Twilio Quest...I think

---
+++ laserPower.js
@@ -0,0 +1,19 @@
+function calculatePower(powerSettings)
+{
+  let totalPower = 0;
+
+    numbers = [];
+
+    powerSettings.forEach(function(number)
+    {
+      totalPower += number*2
+    })
+
+  return totalPower;
+
+}
+  
+  // The following lines of code are not required for the solution, but can be
+  // used by you to test your solution.
+  const laserPower = calculatePower([1, 3, 8]);
+  console.log("Required laser power is " + laserPower); // should be 24
